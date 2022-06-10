f767f8880eb4cc11890167f68e216cb3a0f6f57d
Viskonti <101569662+Viskonti@users.noreply.github.com>
2022-06-10 16:06:54 +0300

    Адаптивчик сайтика
    Очень круто

--- js/fillstaff.js
+++ js/fillstaff.js
@@ -1,16 +1,10 @@
-$('img.img-svg').each(function(){
-  var $img = $(this);
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-  $.get(imgURL, function(data) {
-    var $svg = $(data).find('svg');
-    if(typeof imgClass !== 'undefined') {
-      $svg = $svg.attr('class', imgClass+' replaced-svg');
-    }
-    $svg = $svg.removeAttr('xmlns:a');
-    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-    }
-    $img.replaceWith($svg);
-  }, 'xml');
-});
+let arrayN = document.querySelectorAll(".navbar__menu a");
+hrefClass = function () {
+  for (let j = 0; j < arrayN.length; j++) {
+    arrayN[j].classList.remove("active");
+    this.classList.add("active");
+  }
+};
+for (let j = 0; j < arrayN.length; j++) {
+  arrayN[j].addEventListener("click", hrefClass);
+}
